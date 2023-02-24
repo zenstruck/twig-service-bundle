0b854f01b891e7df87e7f255680033270a9d3d8c
Kevin Bond <kevinbond@gmail.com>
2023-02-24 13:41:12 -0500

    minor: refactor bundle class

--- src/DependencyInjection/ZenstruckTwigServiceExtension.php
+++ src/DependencyInjection/ZenstruckTwigServiceExtension.php
@@ -16,8 +16,11 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\DependencyInjection\Argument\ServiceLocatorArgument;
 use Symfony\Component\DependencyInjection\Argument\TaggedIteratorArgument;
 use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Zenstruck\Twig\AsTwigFunction;
 use Zenstruck\Twig\AsTwigService;
 use Zenstruck\Twig\Service\TwigFunctionRuntime;
 use Zenstruck\Twig\Service\TwigServiceExtension;
@@ -27,8 +30,10 @@ use Zenstruck\Twig\Service\TwigServiceRuntime;
  * @author Kevin Bond <kevinbond@gmail.com>
  *
  * @internal
+ *
+ * @phpstan-import-type Function from TwigFunctionRuntime
  */
-final class ZenstruckTwigServiceExtension extends ConfigurableExtension implements ConfigurationInterface
+final class ZenstruckTwigServiceExtension extends ConfigurableExtension implements ConfigurationInterface, CompilerPassInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
@@ -50,6 +55,87 @@ final class ZenstruckTwigServiceExtension extends ConfigurableExtension implemen
         ;
 
         return $builder;
+    }
+
+    public function process(ContainerBuilder $container): void
+    {
+        $container->getDefinition('parameter_bag')
+            ->addTag('twig.service', ['alias' => TwigServiceRuntime::PARAMETER_BAG])
+        ;
+
+        /** @var array<string,Function> $userFunctions */
+        $userFunctions = $container->getParameter('zenstruck_twig_service.functions');
+
+        $container->getParameterBag()->remove('zenstruck_twig_service.functions');
+
+        foreach ($userFunctions as $value) {
+            if (\is_callable($value)) {
+                continue;
+            }
+
+            $definition = $container->findDefinition($value[0]);
+
+            if (!$definition->hasTag('twig.service_method')) {
+                $definition->addTag('twig.service_method');
+            }
+        }
+
+        foreach ($container->getDefinitions() as $id => $definition) {
+            if (!$class = $definition->getClass()) {
+                continue;
+            }
+
+            if (!\class_exists($class)) {
+                continue;
+            }
+
+            $addTag = false;
+
+            foreach ((new \ReflectionClass($class))->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+                if (!$attribute = $method->getAttributes(AsTwigFunction::class)[0] ?? null) {
+                    continue;
+                }
+
+                $alias = $attribute->newInstance()->alias ?? $method->name;
+
+                if (isset($userFunctions[$alias])) {
+                    throw new LogicException(\sprintf('The function alias "%s" is already being used for "%s".', $alias, \is_string($userFunctions[$alias]) ? $userFunctions[$alias] : \implode('::', $userFunctions[$alias])));
+                }
+
+                if ($method->isStatic()) {
+                    $userFunctions[$alias] = [$class, $method->name];
+
+                    continue;
+                }
+
+                $addTag = true;
+                $userFunctions[$alias] = [$id, $method->name];
+            }
+
+            if ($addTag) {
+                $definition->addTag('twig.service_method', ['alias' => $id]);
+            }
+        }
+
+        foreach (\get_defined_functions()['user'] as $function) {
+            $ref = new \ReflectionFunction($function);
+
+            if (!$attribute = $ref->getAttributes(AsTwigFunction::class)[0] ?? null) {
+                continue;
+            }
+
+            $alias = $attribute->newInstance()->alias ?? $ref->getShortName();
+
+            if (isset($userFunctions[$alias])) {
+                throw new LogicException(\sprintf('The function alias "%s" is already being used for "%s".', $alias, \is_string($userFunctions[$alias]) ? $userFunctions[$alias] : \implode('::', $userFunctions[$alias])));
+            }
+
+            $userFunctions[$alias] = $ref->name;
+        }
+
+        $container->getDefinition('.zenstruck.twig.function_runtime')
+            ->addArgument($userFunctions)
+        ;
     }
 
     public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface // @phpstan-ignore-line
--- src/ZenstruckTwigServiceBundle.php
+++ src/ZenstruckTwigServiceBundle.php
@@ -11,103 +11,11 @@
 
 namespace Zenstruck\Twig;
 
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Zenstruck\Twig\Service\TwigFunctionRuntime;
-use Zenstruck\Twig\Service\TwigServiceRuntime;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
- *
- * @phpstan-import-type Function from TwigFunctionRuntime
  */
-final class ZenstruckTwigServiceBundle extends Bundle implements CompilerPassInterface
+final class ZenstruckTwigServiceBundle extends Bundle
 {
-    public function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass($this);
-    }
-
-    public function process(ContainerBuilder $container): void
-    {
-        $container->getDefinition('parameter_bag')
-            ->addTag('twig.service', ['alias' => TwigServiceRuntime::PARAMETER_BAG])
-        ;
-
-        /** @var array<string,Function> $userFunctions */
-        $userFunctions = $container->getParameter('zenstruck_twig_service.functions');
-
-        foreach ($userFunctions as $value) {
-            if (\is_callable($value)) {
-                continue;
-            }
-
-            $definition = $container->findDefinition($value[0]);
-
-            if (!$definition->hasTag('twig.service_method')) {
-                $definition->addTag('twig.service_method');
-            }
-        }
-
-        $container->getParameterBag()->remove('zenstruck_twig_service.functions');
-
-        foreach ($container->getDefinitions() as $id => $definition) {
-            if (!$class = $definition->getClass()) {
-                continue;
-            }
-
-            if (!\class_exists($class)) {
-                continue;
-            }
-
-            $addTag = false;
-
-            foreach ((new \ReflectionClass($class))->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
-                if (!$attribute = $method->getAttributes(AsTwigFunction::class)[0] ?? null) {
-                    continue;
-                }
-
-                $alias = $attribute->newInstance()->alias ?? $method->name;
-
-                if (isset($userFunctions[$alias])) {
-                    throw new LogicException(\sprintf('The function alias "%s" is already being used for "%s".', $alias, \is_string($userFunctions[$alias]) ? $userFunctions[$alias] : \implode('::', $userFunctions[$alias])));
-                }
-
-                if ($method->isStatic()) {
-                    $userFunctions[$alias] = [$class, $method->name];
-
-                    continue;
-                }
-
-                $addTag = true;
-                $userFunctions[$alias] = [$id, $method->name];
-            }
-
-            if ($addTag) {
-                $definition->addTag('twig.service_method', ['alias' => $id]);
-            }
-        }
-
-        foreach (\get_defined_functions()['user'] as $function) {
-            $ref = new \ReflectionFunction($function);
-
-            if (!$attribute = $ref->getAttributes(AsTwigFunction::class)[0] ?? null) {
-                continue;
-            }
-
-            $alias = $attribute->newInstance()->alias ?? $ref->getShortName();
-
-            if (isset($userFunctions[$alias])) {
-                throw new LogicException(\sprintf('The function alias "%s" is already being used for "%s".', $alias, \is_string($userFunctions[$alias]) ? $userFunctions[$alias] : \implode('::', $userFunctions[$alias])));
-            }
-
-            $userFunctions[$alias] = $ref->name;
-        }
-
-        $container->getDefinition('.zenstruck.twig.function_runtime')
-            ->addArgument($userFunctions)
-        ;
-    }
 }
